0fb61869ae86d9bb80ff1d60aa8c1a5c400ed834
Amauri Lisboa <amauri.olisboa@gmail.com>
2025-04-15 16:27:34 +0100

    added some comments to continue when possible

--- srcs/Commands/kick.cpp
+++ srcs/Commands/kick.cpp
@@ -1,6 +1,11 @@
 #include "../../includes/Server.hpp"
 #include "../../includes/tests.hpp"
 
+
+//Aqui a intencao eh buscar dentro da lista de todos os channels do server
+//e verificar se o server existe
+//tendo em consideracao o nome do servidor a ser procurado
+//(que eh o comando dado no KICK)
 bool	IsAValidChannel(std::map<std::string, Channel*> channels, std::string name)
 {
 	std::cout << "IsAValidChannel Mathod\n";
@@ -16,6 +21,13 @@ bool	IsAValidChannel(std::map<std::string, Channel*> channels, std::string name)
 	return (false);
 }
 
+//Aqui a intencao eh buscar o channel por nome para verificar se o client que esta
+//tentando realizar a acao eh um admin ou nao
+bool	isClientAdmin(Client *client, std::string channel_name)
+{
+
+}
+
 int	Server::kickUser(std::vector<std::string> &cmds, Client *client)
 {
 	std::cout << "kickUser method \n";
@@ -26,7 +38,7 @@ int	Server::kickUser(std::vector<std::string> &cmds, Client *client)
 		//return (does not have correct arguments)
 	if (cmds.size() < 3)
 	{
-		print_container(cmds, "Size < 3");
+		//print_container(cmds, "Size < 3");
 		//Broadcast here msg de kick invalido para todos os usuarios
 		std::cout << "Not a valid KICK command, try KICK #channel targetUser [:reason]" << std::endl;
 		return (EXIT_FAILURE);
@@ -43,20 +55,25 @@ int	Server::kickUser(std::vector<std::string> &cmds, Client *client)
 	//check if channel exist
 	if (IsAValidChannel(this->_channels, cmds[1]))
 	{
-		std::cout << "ENTROU NO IF" << std::endl;
-	}
+		std::cout << "CHANNEL ENCONTRADO" << std::endl;
 		//if yes
+			//check if user to be expelled exists and is in channel
 				//if yes
-					//check if user to be expelled exists and is in channel
-						//if yes
-							//expell the target user from the channel
-							//remove it from channels list
-								//if user is the last of the channel
-									//delete channel??? maybe.
-							//broadcast a messate to the server
-						//if no
-							//error user not in the channel
+					//expell the target user from the channel
+					//remove it from channels list
+						//if user is the last of the channel
+							//delete channel??? maybe.
+					//broadcast a messate to the server
+				//if no
+					//error user not in the channel
+	}
+	else
+	{
 		//if no
-	//return error msg "no channel"
+		//return error msg "no channel"
+		std::cout << "CHANNEL NAO ENCONTRADO\n" << std::endl;
+	}
+		
+		
 	return (EXIT_SUCCESS);
 }
